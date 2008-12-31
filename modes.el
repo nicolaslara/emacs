931a579688334c1e18f50bc36c963d6307b864a3
Nicolas Lara <nicolaslara@gmail.com>
2008-12-31 04:11:34 +0000

    added cedet

--- modes.el
+++
@@ -1,2 +0,0 @@
-(add-to-list 'load-path (concat elisp-root "/modes/git-emacs"))
-(require 'git-emacs)
--- tools.el
+++ tools.el
@@ -1,2 +1,7 @@
+;; load git-emacs
 (add-to-list 'load-path (concat elisp-root "/tools/git-emacs"))
 (require 'git-emacs)
+
+;; load cedet (if experiencing problems, recompile)
+(load-file (concat elisp-root "/tools/cedet/common/cedet.el"))
+(semantic-load-enable-excessive-code-helpers)
