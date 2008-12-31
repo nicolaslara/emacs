9be8d176ed9649b03a28c820efd97e466fddcc64
Nicolas Lara <nicolaslara@gmail.com>
2008-12-31 05:49:14 +0000

    added js2-mode

--- modes.el
+++ modes.el
@@ -11,3 +11,8 @@
       (append '(("\\.txt$" . rst-mode)
                 ("\\.rst$" . rst-mode)
                 ("\\.rest$" . rst-mode)) auto-mode-alist))
+
+;; Loads js2-mode
+(autoload 'js2-mode "js2" nil t)
+(add-to-list 'auto-mode-alist '("\\.js$" . js2-mode)))
+    
