b7e6be617e7d856d3d49d4ee902a7278458dd172
Nicolas Lara <nicolaslara@gmail.com>
2009-02-28 23:38:56 +0000

    htmlize-code-snnipet

--- functions.el
+++ functions.el
@@ -115,3 +115,25 @@
   "Converts buffer to html and writes it to ~/public_html/emacs.html"
   (interactive)
   (publish-buffer-to "~/public_html/emacs.html"))
+
+(defun htmlize-code-snnipet (beg end)
+  "Htmlize region and put into <pre> tag style that is left in <body> tag
+plus add font-size: 8pt"
+  (interactive "r")
+  (let* ((buffer-faces (htmlize-faces-in-buffer))
+         (face-map (htmlize-make-face-map (adjoin 'default buffer-faces)))
+         (pre-tag (format
+                   "<pre style=\"%s font-size: 8pt\">"
+                   (mapconcat #'identity (htmlize-css-specs
+                                          (gethash 'default face-map)) " ")))
+         (htmlized-reg (htmlize-region-for-paste beg end)))
+    (switch-to-buffer-other-window "*htmlized output*")
+    ; clear buffer
+    (kill-region (point-min) (point-max))
+    ; set mode to have syntax highlighting
+    (nxml-mode)
+    (save-excursion
+      (insert htmlized-reg))
+    (while (re-search-forward "<pre>" nil t)
+      (replace-match pre-tag nil nil))
+    (goto-char (point-min))))
